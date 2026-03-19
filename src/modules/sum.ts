1241e41a97b912a08836ae31e56db3d3eca73064
coljurten <louis.dalonis@gmail.com>
2026-03-19 17:16:39 +0100

    feat: :chart_with_upwards_trend: 1st sum function !!!

--- src/modules/sum.ts
+++ src/modules/sum.ts
@@ -9,9 +9,3 @@ export function sum(numA: number, numB: number){
     return sumSmallNumbers(numA, numB);
 };
 
-export function addition(a: number, b: number){
-    if (a  < 0 || b < 0){
-        throw new Error("Your numbers must be positive !")
-    }
-    return a + b;
-};
